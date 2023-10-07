d9260b3e227128faee313412ebe2f351262f20ad
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-10-06 20:52:33 -0700

    moved code to generateMarkdown.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-// const generateMarkdown = require('./utils/generateMarkdown');
-
-const licenseBadges = {
-    'MIT License': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-    'Apache License 2.0': '[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)',
-    'GNU General Public License (GPL) 3.0':'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-    'BSD 3-Clause "New" or "Revised" License':'[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
-}
+const {generateMarkdown, licenseBadges} = require('./utils/generateMarkdown');
 
 const questions = [
     {
@@ -76,50 +69,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((answers) => {
         const badge = licenseBadges[answers.license] || '';
-        let readmeTemplate = 
-        `# ${answers.projectTitle} ${badge}
-    
-## Description 
-    
-${answers.projectDescription}
-    
-## Table of contents
-- [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-- [License](#license)
-    
-## Installation
-    
-${answers.installation}
-    
-## Usage
-    
-${answers.usage}
-    
-## Contributing
-    
-${answers.contributing}
-    
-## Tests
-    
-${answers.tests}
-    
-## Questions
-    
-GitHub Profile: https://github.com/${answers.githubUsername}
-
-Email: ${answers.email}
-    
-## License
-
-${answers.license === 'None' ? '' : `This project is licensed under the terms of the ${answers.license}`}
-
-`;
-    
+        const readmeTemplate = generateMarkdown(answers,badge);
         writeToFile('README.md',readmeTemplate);
     });
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,54 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseBadges = {
+  'MIT License': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  'Apache License 2.0': '[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)',
+  'GNU General Public License (GPL) 3.0':'[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+  'BSD 3-Clause "New" or "Revised" License':'[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)',
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers, badge) {
+  return `# ${answers.projectTitle} ${badge}
+    
+## Description 
+      
+${answers.projectDescription}
+      
+## Table of contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+- [License](#license)
+      
+## Installation
+      
+${answers.installation}
+      
+## Usage
+      
+${answers.usage}
+      
+## Contributing
+      
+${answers.contributing}
+      
+## Tests
+      
+${answers.tests}
+      
+## Questions
+  
+GitHub Profile: https://github.com/${answers.githubUsername}
+  
+Best way to reach me is through email, ${answers.email}.
+      
+## License
+  
+ ${answers.license === 'None' ? '' : `This project is licensed under the terms of the ${answers.license}`} 
 
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {generateMarkdown, licenseBadges};
